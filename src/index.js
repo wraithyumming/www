e0542226ff59894e06c798d096eab31a61a30a10
bladee <freedum0@gmail.com>
2025-03-05 18:15:53 +0200

    ?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
 // Использование React.createElement для создания элемента
 // ReactDOM.render(React.createElement('input', {
 //     placeholder: "Help text",
@@ -9,7 +12,19 @@
 const inputClick = () => console.log("Clicked");
 const mouseOver = () => console.log("Mouse Over");
 
-const elements = <input placeholder="Help text" onClick={inputClick} onMouseEnter={mouseOver} />;
+const helpText = "Help text";
+
+const elements = (
+    <div className="name">
+        <h1>{helpText}</h1>
+        <input
+            placeholder={helpText}
+            onClick={inputClick}
+            onMouseEnter={mouseOver}
+        />
+        <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
+    </div>
+);
 
 const app = document.getElementById("app");
 
