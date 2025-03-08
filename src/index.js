bba45089650925e52dcd0eed98808c2c06898ad6
bladee <freedum0@gmail.com>
2025-03-08 18:06:05 +0200

    new component

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ import React from 'react'
 import * as ReactDOMClient from 'react-dom/client';
 
 
-// Использование React.createElement для создания элемента
-// ReactDOM.render(React.createElement('input', {
-//     placeholder: "Help text",
-//     onClick: () => console.log("Clicked"),
-//     onMouseEnter: () => console.log("Mouse Over")
-// }), document.getElementById("app"));
 
-// Использование JSX для создания элемента
-const inputClick = () => console.log("Clicked");
-const mouseOver = () => console.log("Mouse Over");
-
-const helpText = "Help text";
-
-class App extends React.Component {
+class Header extends React.Component {
     render() {
         return (
             <header>Шапка сайта</header>
@@ -24,18 +12,22 @@ class App extends React.Component {
 }
 
 class App extends React.Component {
+    helpText = "Help text";
     render() {
-    return (<div className="name">
-        <Header />
-        <h1>{helpText}</h1>
-        <input
-            placeholder={helpText}
-            onClick={inputClick}
-            onMouseEnter={mouseOver}
-        />
-        <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
-    </div>)
-}}
+        return (<div className="name">
+            <Header />
+            <h1>{this.helpText}</h1>
+            <input
+                placeholder={this.helpText}
+                onClick={this.inputClick}
+                onMouseEnter={this.mouseOver}
+            />
+            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+        </div>)
+    }
+    inputClick() { console.log("Clicked") }
+    mouseOver() { console.log("Mouse Over") }
+}
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
