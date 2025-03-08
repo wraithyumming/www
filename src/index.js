581932f7fbfeaca64a9a3340d3b375bf2d88d6c7
bladee <freedum0@gmail.com>
2025-03-08 18:24:09 +0200

    add css component

--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client';
-
-
-
-class Header extends React.Component {
-    render() {
-        return (
-            <header>{this.props.title}</header>
-        )
-    }
-}
-
-class App extends React.Component {
-    helpText = "Help text";
-    render() {
-        return (<div className="name">
-            <Header title="Шапка сайта" />
-            <h1>{this.helpText}</h1>
-            <input
-                placeholder={this.helpText}
-                onClick={this.inputClick}
-                onMouseEnter={this.mouseOver}
-            />
-            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
-        </div>)
-    }
-    inputClick() { console.log("Clicked") }
-    mouseOver() { console.log("Mouse Over") }
-}
+import App from './App'
+import './css/main.css'
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
