d6d67b3f8d18d1778710ebb7837a4ce6374dcef2
bladee <freedum0@gmail.com>
2025-03-23 14:09:30 +0200

    remove/ create project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
-import Image from './components/Image';
-import logo from './img/34.jpg';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            helpText: "Help text",
-            userData: "",
-        };
-
-        this.inputClick = this.inputClick.bind(this);
-    }
 
     render() {
         return (
-            <div className="name">
-                <Header title="Шапка сайта" name="" />
-                <h1>{this.state.helpText}</h1>
-                <h2>{this.state.userData}</h2>
-                <input
-                    placeholder={this.state.helpText}
-                    onChange={event => this.setState({ userData: event.target.value })}
-                    onClick={this.inputClick}
-                    onMouseEnter={this.mouseOver}
-                />
-                <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
-                <Image image={logo} />
-                <img src={logo} alt="logo" />
+            <div>
+                <Header title="Список пользователей" />
+
             </div>
         );
     }
 
-    inputClick() {
-        this.setState({ helpText: "Changed" });
-        console.log("Clicked");
-    }
 
-    mouseOver() {
-        console.log("Mouse Over");
-    }
 }
 
 export default App;
