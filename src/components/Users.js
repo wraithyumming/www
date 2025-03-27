e56977aa432bd242447728737ee1feeb170580b0
bladee <freedum0@gmail.com>
2025-03-27 16:55:57 +0200

    checking for users

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,12 +20,18 @@ class Users extends React.Component {
         }
     ]
     render() {
-        return (<div>
-            {this.users.map((el)=>(<div>
-                <h3>{el.firstname} {el.lastname}</h3>
-                <p>{el.bio}</p>
-                </div> ))}
-        </div>)
+        if (this.users.length > 0)
+            return (<div>
+                {this.users.map((el) => (<div className="user" key={el.id}>
+                    <h3>{el.firstname} {el.lastname}</h3>
+                    <p>{el.bio}</p>
+                </div>))}
+            </div>)
+            else
+            return (<div className="user">
+                <h3>Пользователей нет</h3>
+
+            </div>)
     }
 
 }
