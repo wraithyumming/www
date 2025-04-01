afb6354be38a9e9cde41471ec395c903d8aa7236
bladee <freedum0@gmail.com>
2025-04-01 16:13:53 +0300

    add forms

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,34 +1,39 @@
 import React from 'react'
 
 class Users extends React.Component {
-    users = [
-        {
-            "id": 1,
-            "firstname": "Bob",
-            "lastname": "Marley",
-            "bio": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repella",
-            "age": 40,
-            "isHappy": true
-        },
-        {
-            "id": 2,
-            "firstname": "John",
-            "lastname": "Doe",
-            "bio": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repella",
-            "age": 22,
-            "isHappy": false
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    "id": 1,
+                    "firstname": "Bob",
+                    "lastname": "Marley",
+                    "bio": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repella",
+                    "age": 40,
+                    "isHappy": true
+                },
+                {
+                    "id": 2,
+                    "firstname": "John",
+                    "lastname": "Doe",
+                    "bio": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repella",
+                    "age": 22,
+                    "isHappy": false
+                }
+            ]
         }
-    ]
+    }
     render() {
         if (this.users.length > 0)
             return (<div>
                 {this.users.map((el) => (<div className="user" key={el.id}>
                     <h3>{el.firstname} {el.lastname}</h3>
                     <p>{el.bio}</p>
-                    <b>{el.isHappy ? 'Счастлив!': 'Не особо:('}</b>
+                    <b>{el.isHappy ? 'Счастлив!' : 'Не особо:('}</b>
                 </div>))}
             </div>)
-            else
+        else
             return (<div className="user">
                 <h3>Пользователей нет</h3>
 
