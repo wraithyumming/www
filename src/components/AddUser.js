23a3afcad9ca644115dc87b2c8d98acf9d214918
bladee <freedum0@gmail.com>
2025-04-07 15:09:42 +0300

    deleted list

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -13,20 +13,24 @@ class AddUser extends React.Component {
     }
     render() {
         return (
-            <form>
+            <form ref={(el) => this.myForm = el}>
                 <input placeholder='Имя' onChange={(e) => this.setState({ firstname: e.target.value })} />
                 <input placeholder='Фамилия' onChange={(e) => this.setState({ lastname: e.target.value })} />
                 <textarea placeholder='Биография' onChange={(e) => this.setState({ bio: e.target.value })} />
                 <input placeholder='Возраст' onChange={(e) => this.setState({ age: e.target.value })} />
                 <label htmlForm="isHappy">Счастлив?</label>
                 <input type="checkbox" id="isHappy" onChange={(e) => this.setState({ isHappy: e.target.checked })} />
-                <button type="button" onClick={() => this.props.onAdd({
+                <button type="button" onClick={() => {
+                   this.myForm.reset()
+                   this.props.onAdd({
                     firstname: this.state.firstname,
                     lastname: this.state.lastname,
                     bio: this.state.bio,
                     age: this.state.age,
-                    isHappy: this.state.isHappy
-                })}>Добавить</button>
+                    isHappy: this.state.isHappy,
+                })
+                }
+                }>Добавить</button>
             </form>
         )
     }
